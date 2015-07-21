d15e1ad9a0c83f358477ea21e5ab44caa901cea2
liyuanbo <liyuanbo@liyuanbodeMacBook-Pro.local>
2015-07-21 22:06:19 +0800

    111

---
+++ iwanerPro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iwanerPro
+//
+//  Created by liyuanbo on 7/21/15.
+//  Copyright (c) 2015 iwaner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iwanerPro/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  iwanerPro
+//
+//  Created by liyuanbo on 7/21/15.
+//  Copyright (c) 2015 iwaner. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
